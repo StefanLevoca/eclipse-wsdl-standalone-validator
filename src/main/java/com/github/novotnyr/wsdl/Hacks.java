1b240d03d5c16470696ab6014cce50f688ed7c12
Robert Novotny <novotnr0@gmail.com>
2019-10-15 10:16:59 +0200

    Remove exploratory code without purpose

--- src/main/java/com/github/novotnyr/wsdl/Hacks.java
+++
@@ -1,8 +0,0 @@
-package com.github.novotnyr.wsdl;
-
-
-public class Hacks {
-    public static void initializeEntryTypeMap() {
-
-    }
-}
--- src/main/java/com/github/novotnyr/wsdl/Main.java
+++ src/main/java/com/github/novotnyr/wsdl/Main.java
@@ -18,15 +18,13 @@ import org.eclipse.wst.wsi.internal.core.util.EntryType;
 import org.eclipse.wst.wsi.internal.core.util.Utils;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         if (args.length == 0) {
             System.err.println("Missing file argument");
             System.exit(2);
@@ -38,77 +36,66 @@ public class Main {
         }
 
 
-        try(InputStream wsdlInputStream = new FileInputStream(wsdlFile)) {
-            WSDLValidator validator = new WSDLValidator();
-            //validator.addURIResolver(new URIResolverWrapper());
+        WSDLValidator validator = new WSDLValidator();
 
-            WSDLValidatorDelegate wsiDelegate = new ClassloaderWSDLValidatorDelegate("org.eclipse.wst.wsi.internal.validate.wsdl.WSDLValidator");
-            validator.registerWSDLExtensionValidator("http://schemas.xmlsoap.org/wsdl/", wsiDelegate);
+        WSDLValidatorDelegate wsiDelegate = new ClassloaderWSDLValidatorDelegate("org.eclipse.wst.wsi.internal.validate.wsdl.WSDLValidator");
+        validator.registerWSDLExtensionValidator("http://schemas.xmlsoap.org/wsdl/", wsiDelegate);
 
-            // initialize static singletons
-            String name = "Attachments Profile [1.0] (with Basic Profile [1.1] and Simple Soap Binding Profile [1.0]) Test Assertions";
-            String version = "1.0.0.2";
+        // initialize static singletons
+        String name = "Attachments Profile [1.0] (with Basic Profile [1.1] and Simple Soap Binding Profile [1.0]) Test Assertions";
+        String version = "1.0.0.2";
 
-            Utils.registerValidProfileTADVersion(name, version);
+        Utils.registerValidProfileTADVersion(name, version);
 
-            WSITestToolsPlugin wsiTestToolsPlugin = new WSITestToolsPlugin();
-            WSITestToolsProperties.setEclipseContext(false);
+        WSITestToolsPlugin wsiTestToolsPlugin = new WSITestToolsPlugin();
+        WSITestToolsProperties.setEclipseContext(false);
 
-            ArtifactType.registerArtifactType("discovery");
-            ArtifactType.registerArtifactType("description");
-            ArtifactType.registerArtifactType("message");
-            ArtifactType.registerArtifactType("envelope");
+        ArtifactType.registerArtifactType("discovery");
+        ArtifactType.registerArtifactType("description");
+        ArtifactType.registerArtifactType("message");
+        ArtifactType.registerArtifactType("envelope");
 
-            WSDLValidatorImpl wsdlValidator = new WSDLValidatorImpl();
-            MessageValidatorImpl messageValidator = new MessageValidatorImpl();
-            EnvelopeValidatorImpl envelopeValidator = new EnvelopeValidatorImpl();
-            UDDIValidatorImpl uddiValidator = new UDDIValidatorImpl();
+        WSDLValidatorImpl wsdlValidator = new WSDLValidatorImpl();
+        MessageValidatorImpl messageValidator = new MessageValidatorImpl();
+        EnvelopeValidatorImpl envelopeValidator = new EnvelopeValidatorImpl();
+        UDDIValidatorImpl uddiValidator = new UDDIValidatorImpl();
 
 
-            ProfileValidatorFactoryImpl.addToValidatatorRegistry("discovery", uddiValidator);
-            ProfileValidatorFactoryImpl.addToValidatatorRegistry("description", wsdlValidator);
-            ProfileValidatorFactoryImpl.addToValidatatorRegistry("message", messageValidator);
-            ProfileValidatorFactoryImpl.addToValidatatorRegistry("envelope", envelopeValidator);
+        ProfileValidatorFactoryImpl.addToValidatatorRegistry("discovery", uddiValidator);
+        ProfileValidatorFactoryImpl.addToValidatatorRegistry("description", wsdlValidator);
+        ProfileValidatorFactoryImpl.addToValidatatorRegistry("message", messageValidator);
+        ProfileValidatorFactoryImpl.addToValidatatorRegistry("envelope", envelopeValidator);
 
-            List<BaseValidator> validators = new ArrayList<>();
-            validators.add(wsdlValidator);
-            validators.add(messageValidator);
-            validators.add(envelopeValidator);
-            validators.add(uddiValidator);
+        List<BaseValidator> validators = new ArrayList<>();
+        validators.add(wsdlValidator);
+        validators.add(messageValidator);
+        validators.add(envelopeValidator);
+        validators.add(uddiValidator);
 
+        try {
+            Field validatorsField = WSITestToolsPlugin.class.getDeclaredField("validators");
+            validatorsField.setAccessible(true);
+            validatorsField.set(wsiTestToolsPlugin, validators.toArray(new BaseValidator[0]));
 
-
-            try {
-                Field validatorsField = WSITestToolsPlugin.class.getDeclaredField("validators");
-                validatorsField.setAccessible(true);
-                validatorsField.set(wsiTestToolsPlugin, validators.toArray(new BaseValidator[0]));
-
-                Field reportArtifactTypesField = WSITestToolsPlugin.class.getDeclaredField("reportArtifactTypes");
-                reportArtifactTypesField.setAccessible(true);
-                // see org.eclipse.wst.wsi_1.0.650.v201904232049.jar!/plugin.xml:81
-                reportArtifactTypesField.set(wsiTestToolsPlugin, new String[] { "message" });
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            //ArtifactType.registerArtifactType("NULL");
-            //EntryType.registerEntryType(ArtifactType.getArtifactType("NULL"), "NULL");
-
-            for (BaseValidator v : validators) {
-                for (String entryType : v.getEntryTypes()) {
-                    EntryType.registerEntryType(ArtifactType.getArtifactType(v.getArtifactType()), entryType);
-                }
-            }
-
-            IValidationReport report = validator.validate(wsdlFile.toURI().toURL().toString(), null, null);
-
-            for (IValidationMessage validationMessage : report.getValidationMessages()) {
-                System.out.println(validationMessage.getLine() + ":" + validationMessage.getColumn() + "\t" + validationMessage.getMessage());
-            }
-        } catch (IOException e) {
+            Field reportArtifactTypesField = WSITestToolsPlugin.class.getDeclaredField("reportArtifactTypes");
+            reportArtifactTypesField.setAccessible(true);
+            // see org.eclipse.wst.wsi_1.0.650.v201904232049.jar!/plugin.xml:81
+            reportArtifactTypesField.set(wsiTestToolsPlugin, new String[] { "message" });
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+        for (BaseValidator v : validators) {
+            for (String entryType : v.getEntryTypes()) {
+                EntryType.registerEntryType(ArtifactType.getArtifactType(v.getArtifactType()), entryType);
+            }
+        }
 
+        IValidationReport report = validator.validate(wsdlFile.toURI().toURL().toString(), null, null);
+
+        for (IValidationMessage validationMessage : report.getValidationMessages()) {
+            System.out.println(validationMessage.getLine() + ":" + validationMessage.getColumn() + "\t" + validationMessage.getMessage());
+        }
     }
 }
