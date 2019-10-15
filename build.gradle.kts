8480f3d6cec5d61ea8e2a9c5102152238a8c7915
Robert Novotny <novotnr0@gmail.com>
2019-10-15 10:13:23 +0200

    Migrate to Gradle builds

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+    mavenLocal()
+}
+
+dependencies {
+    implementation(fileTree("lib"))
+
+    implementation("xerces:xercesImpl:2.12.0")
+    implementation("wsdl4j:wsdl4j:1.6.3")
+    implementation("com.ibm.icu:icu4j:64.2")
+}
